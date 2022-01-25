167f9673bd486f1535dea3d41f2d1631c3bb584b
adelazamfirescu <adela.zamfirescu@vodafone.com>
2022-01-25 17:07:32 +0200

    added fetch in index component

--- blog-client/src/components/ArticlePreview.js
+++ blog-client/src/components/ArticlePreview.js
@@ -1,25 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import ArticleHeader from "../components/ArticleHeader";
 import ContentPreview from "../components/ContentPreview";
 import ButtonReadmore from "../components/buttons/ButtonReadmore";
 
 class ArticlePreview extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { articleId: this.props.article.articleId, headerData: this.props.article.headerData, content: this.props.article.content};
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      article: {
+        id: this.props.article.id,
+        title: this.props.article.title,
+        tag: this.props.article.tag,
+        author: this.props.article.author,
+        date: this.props.article.date,
+        imgUrl: this.props.article.imgUrl,
+        saying: this.props.article.saying,
+        content: this.props.article.content,
+      },
+    };
+  }
 
-    
   render() {
     return (
       <article>
-        <ArticleHeader headerData={this.state.headerData}/>
-        <ContentPreview content={this.state.content}/>
-        <ButtonReadmore articleId={this.state.articleId} />
-    </article>
-    )
+        <ArticleHeader
+          headerData={
+            (this.article.title,
+            this.article.tag,
+            this.article.author,
+            this.article.date,
+            this.article.imgUrl)
+          }
+        />
+        <ContentPreview content={this.article.content} />
+        <ButtonReadmore articleId={this.article.id} />
+      </article>
+    );
   }
 }
-
 
 export default ArticlePreview;
--- blog-client/src/pages/Index.js
+++ blog-client/src/pages/Index.js
@@ -9,21 +9,71 @@ import ArticlePreview from "../components/ArticlePreview";
 class Index extends Component {
   constructor(props) {
     super(props);
-    this.state = { article: 
-      {articleId: "2465d861-98ad-492b-99c9-ace00cb64204", 
-      headerData:
-      {title: "Transilvania", tag:"Asdf", author:"gfdf", date:"21.12.345",  imgUrl:"../public/assets/img/bike.jpg"}, 
-      content: ['asssjjfjfdjdjsdjdjjdjjds', 'jsjdjjdjdjdjfhjejiejenifeif'] }}}
+    this.state = {
+      numberOfArticlesPerPage: 4,
+      indexStart: 0,
+      indexEnd: this.numberOfArticlesPerPage - 1,
 
-  
+      totalNumberOfArticles: 0,
+      articleList: [],
+      article: {
+        articleId: "2465d861-98ad-492b-99c9-ace00cb64204",
+        headerData: {
+          title: "Transilvania",
+          tag: "Asdf",
+          author: "gfdf",
+          date: "21.12.345",
+          imgUrl: "../public/assets/img/bike.jpg",
+        },
+        content: ["asssjjfjfdjdjsdjdjjdjjds", "jsjdjjdjdjdjfhjejiejenifeif"],
+      },
+    };
+    this.getArticleList = this.getArticleList.bind(this);
+  }
+
+  // TAKING DATA FROM SERVER
+  getArticleList() {
+    const self = this;
+
+    fetch(
+      `http://localhost:3007/articles?indexStart=${this.indexStart}&indexEnd=${this.indexEnd}`
+    )
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function (data) {
+          self.setState({
+            articleList: data.articlesList,
+            totalNumberOfArticles: data.numberOfArticles,
+          });
+        });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+  }
+
+  componentDidMount() {
+    this.getArticleList();
+  }
 
   render() {
+    const { articleList } = this.state;
+    const articles = articleList.map((article) => (
+      <ArticlePreview article={article} key={article.id} />
+    ));
     return (
       <>
         <ThemeSwitch />
         <NavBar />
         <ButtonModal />
-        <ArticlePreview article={this.state.article} />
+        {articles}
       </>
     );
   }
