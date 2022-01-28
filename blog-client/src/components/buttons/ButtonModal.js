856e2b6df6dfb1f89eb3fe2f258160c5ede2e44c
adelazamfirescu <adela.zamfirescu@vodafone.com>
2022-01-28 17:16:08 +0200

    changed to functional component

--- blog-client/src/components/buttons/ButtonModal.js
+++ blog-client/src/components/buttons/ButtonModal.js
@@ -1,28 +1,17 @@
-import React, { Component } from "react";
-import Index from "../../pages/Index";
+import React from "react";
 
-import { Link } from "react-router-dom";
-
-class ButtonModal extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-
-
-  render() {
-    return (
-      <div className="add__container">
-        <button
-          onClick={this.props.openModal}
-          type="button"
-          className="button open-modal fas fa-plus"
-        >
-          Add article
-        </button>
-      </div>
-    );
-  }
+function ButtonModal({ openModal }) {
+  return (
+    <div className="add__container">
+      <button
+        onClick={openModal}
+        type="button"
+        className="button open-modal fas fa-plus"
+      >
+        Add article
+      </button>
+    </div>
+  );
 }
 
 export default ButtonModal;
