1b756aed6009c2dff170858968be9949ca9f2d28
Ionela <ionela.ciobanasu@vodafone.com>
2022-01-31 11:42:41 +0200

    implemented dark mode functionality

--- blog-client/src/components/ThemeSwitch.js
+++ blog-client/src/components/ThemeSwitch.js
@@ -1,40 +1,38 @@
 import React, {Component} from "react";
-
 class ThemeSwitch extends Component {
     constructor(props) {
         super(props);
-    this.state = {}
+    this.state = {currentTheme: localStorage.getItem('theme') ? localStorage.getItem('theme') : null, body: document.querySelector('body')}
+    this.switchTheme = this.switchTheme.bind(this);
     }
 
-     switchTheme(e) {
-        // if (e.target.checked) {
-        //     body.setAttribute('data-theme', 'dark');
-        //     localStorage.setItem('theme', 'dark');
-        //     changeImageNotFound('dark');
+    switchTheme(event) {
+        if (event.target.checked) {
+          this.state.body.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark');
     
-        // } else {
-        //     body.setAttribute('data-theme', 'light');
-        //     localStorage.setItem('theme', 'light');
-        //     changeImageNotFound('light');
-        // }
+      } else {
+          this.state.body.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+      }
     }
     
-     changeImageNotFound(theme) {
-        // let img = document.getElementById('error-box');
-        // if (img) {
-        //     if (theme === 'dark') {
-        //         img.style.backgroundImage = 'url("/img/Valley-dark.jpg")';
-        //     } else {
-        //         img.style.backgroundImage = 'url("/img/Valley-light.jpg")';
-        //     }
-        // }
-    }
+   
+componentDidMount() {
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
+    if (this.state.currentTheme) {
+        this.state.body.setAttribute('data-theme', this.state.currentTheme);
+    if (this.state.currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+        }
+    }   
+}
 
     render() {
         return (
             <div className="theme-switch-box">
                 <label className="theme-switch" htmlFor="switch">
-                    <input type="checkbox" id="switch"/>
+                    <input type="checkbox" id="switch" onChange={this.switchTheme}/>
                     <div className="icons">
                         <div className="far fa-moon round"></div>
                         <div className="fas fa-sun round"></div>
