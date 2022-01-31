cfa677f24b09afe97ff5fcde8fe930af0e55137f
Oana Giorgi <oana.giorgi@vodafone.com>
2022-01-31 16:40:30 +0200

    404 design page fixed, change image on dark/light

--- blog-client/src/components/ThemeSwitch.js
+++ blog-client/src/components/ThemeSwitch.js
@@ -1,42 +1,41 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 class ThemeSwitch extends Component {
     constructor(props) {
         super(props);
-    this.state = {currentTheme: localStorage.getItem('theme') ? localStorage.getItem('theme') : null, body: document.querySelector('body')}
-    this.switchTheme = this.switchTheme.bind(this);
+        this.state = { currentTheme: localStorage.getItem('theme') ? localStorage.getItem('theme') : null, body: document.querySelector('body') }
+        this.switchTheme = this.switchTheme.bind(this);
     }
 
     switchTheme(event) {
         if (event.target.checked) {
-          this.state.body.setAttribute('data-theme', 'dark');
-          localStorage.setItem('theme', 'dark');
-    
-      } else {
-          this.state.body.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-      }
-    }
-    
-   
-componentDidMount() {
-    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
-    if (this.state.currentTheme) {
-        this.state.body.setAttribute('data-theme', this.state.currentTheme);
-    if (this.state.currentTheme === 'dark') {
-            toggleSwitch.checked = true;
+            this.state.body.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            this.state.body.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
         }
-    }   
-}
+    }
+
+
+    componentDidMount() {
+        const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
+        if (this.state.currentTheme) {
+            this.state.body.setAttribute('data-theme', this.state.currentTheme);
+            if (this.state.currentTheme === 'dark') {
+                toggleSwitch.checked = true;
+            }
+        }
+    }
 
     render() {
         return (
             <div className="theme-switch-box">
                 <label className="theme-switch" htmlFor="switch">
-                    <input type="checkbox" id="switch" onChange={this.switchTheme}/>
+                    <input type="checkbox" id="switch" onChange={this.switchTheme} />
                     <div className="icons">
                         <div className="far fa-moon round"></div>
                         <div className="fas fa-sun round"></div>
-                    </div>     
+                    </div>
                 </label>
             </div>
         )
--- blog-client/src/pages/NotFound.js
+++ blog-client/src/pages/NotFound.js
@@ -6,24 +6,48 @@ import ThemeSwitch from "../components/ThemeSwitch";
 class NotFound extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentTheme: '',
+      currentImg: ''
+    }
   }
 
+  componentDidMount() {
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+    const currentImg = (currentTheme == 'dark' ? 'dark-img' : 'light-img')
+    this.setState({ currentTheme: currentTheme, currentImg: currentImg });
+  }
+
+  // componentDidUpdate(prevState) {
+  //   // compare props:
+  //   if (this.state.currentTheme != prevState.currentTheme) {
+  //     if (this.state.currentTheme == 'dark') {
+  //       this.setState({
+  //         currentImg: "dark-img"
+  //       });
+  //     } else {
+  //       this.setState({
+  //         currentImg: "light-img"
+  //       });
+  //     }
+  //   }
+  // }
+
   render() {
-    return(
+    return (
       <>
-      <ThemeSwitch />
-      <NavBar />
-     
-      <div className="error-box" id="error-box">
-        <div className="error-info">
-          <h1 className="error-message">Error 404 - Page not found!</h1>
-          <Link to={'/'}>
-            <button type="button" className="to-homepage">BACK TO HOMEPAGE</button>
+        <ThemeSwitch />
+        <NavBar />
+        <div className={
+          "error-box " + this.state.currentImg} id="error-box">
+          <div className="error-info">
+            <h1 className="error-message">Error 404 - Page not found!</h1>
+            <Link to={'/'}>
+              <button type="button" className="to-homepage">BACK TO HOMEPAGE</button>
             </Link>
           </div>
         </div>
-       </>
+      </>
     )
   }
 }
